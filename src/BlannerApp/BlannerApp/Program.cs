c9c11244eeb78ccdfd617eafd2e8e325e99dc1f1
Israa AL-Haddad <75621536+isrra-111@users.noreply.github.com>
2025-06-29 13:46:22 +0300

    Create the Api Response models and init the HttpClient instanse

--- src/BlannerApp/BlannerApp/Program.cs
+++ src/BlannerApp/BlannerApp/Program.cs
@@ -1,4 +1,5 @@
 using BlannerApp;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -6,8 +7,16 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddHttpClient("PlannerApp.Api" , client =>
+{
+    client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+ 
+builder.Services.AddTransient<AuthorizationMessageHandler>();
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
 
 await builder.Build().RunAsync();
